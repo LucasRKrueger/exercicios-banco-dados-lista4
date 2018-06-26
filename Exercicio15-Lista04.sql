664f8c914ab8068030b729500a94a42de9eb6253
LucasRodrigoK <lucasrkthd@gmail.com>
2018-06-26 13:37:06 -0300

    Exercicio15-Lista04

---
+++ Exercicio15-Lista04.sql
@@ -0,0 +1,1 @@
+﻿SELECT nome, nota_1, nota_2,nota_3,nota_4,(nota_1 + nota_2 + nota_3 + nota_4)/4'Média' FROM alunos WHERE (nota_1 + nota_2 + nota_3 + nota_4)/4 = (SELECT MIN((nota_1 + nota_2 + nota_3 + nota_4)/4) FROM alunos);
